effa8ddb8a0e6bc212904ea722c0d4e618744319
user2 <user2@user2-PC>
2019-01-10 20:06:48 +0900

    왜안나!!

--- gitTest/src/get/test/Main.java
+++ gitTest/src/get/test/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		 			
+		System.out.println("추가한내용!!!");
 		System.out.println("두번째 수정");
 		System.out.println("이건 충돌이 나야 해 !!!");
 		  
