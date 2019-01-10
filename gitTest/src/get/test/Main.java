25fa12f0a8b90797a42a822124f7b37bb0e4f07b
user2 <user2@user2-PC>
2019-01-10 20:08:14 +0900

    충돌나라!

--- gitTest/src/get/test/Main.java
+++ gitTest/src/get/test/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		 			
 		System.out.println("추가한내용!!!");
+		String name="유병승";
 		System.out.println("두번째 수정");
 		System.out.println("이건 충돌이 나야 해 !!!");
 		  
