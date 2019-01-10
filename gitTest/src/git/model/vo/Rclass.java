2f707d90e39f9f9118f9e1d6c06dbfab530835a4
user2 <user2@user2-PC>
2019-01-10 21:14:47 +0900

    Rclass생성자추가

--- gitTest/src/git/model/vo/Rclass.java
+++ gitTest/src/git/model/vo/Rclass.java
@@ -5,5 +5,17 @@ public class Rclass {
 	private String name="rclass";
 	private int su=21;
 	
+	public Rclass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Rclass(String name, int su) {
+		super();
+		this.name = name;
+		this.su = su;
+	}
+	
+	
+	
 	
 }
